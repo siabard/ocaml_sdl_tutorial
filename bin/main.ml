e4980bf3d166c1f65e2bf6ccb0d09cc7189c4c4f
Yeonho Jang <siabard@gmail.com>
2022-01-24 22:16:26 +0900

    Image atlas 만들기

--- bin/main.ml
+++ bin/main.ml
@@ -3,7 +3,15 @@ open Sdlevent
 module Image = Sdlimage
 
 module SpriteMap = Map.Make(String)
-    
+
+
+type sprite_cell = {
+    x: int;
+    y: int;
+    w: int;
+    h: int;
+  }
+
                 
 let event_loop () =
   match Event.poll_event () with
@@ -22,7 +30,21 @@ let load_sprite renderer sprite_map id  filename =
   Surface.free surf;
   (sprite_map)
 
+
+let make_atlas width height tile_width tile_height =
+  let num_x = width / tile_width in
+  let num_y = height / tile_height in
+  let length = num_x * num_y in
+  let atlas = Array.make length {x = 0; y = 0; w = tile_width; h = tile_height } in
+  for ny = 0 to num_y - 1 do
+    for nx = 0 to num_x - 1 do
+      atlas.(ny * num_x + nx) <- {x = nx * tile_width; y = ny * tile_height; w = tile_width; h = tile_height}
+    done;
+  done;
+  atlas
+
   
+
 
 let main () =
   let width, height = (640, 480) in
@@ -39,11 +61,13 @@ let main () =
   in
   let sprite_map = SpriteMap.empty in
   let sprite_map = load_sprite renderer sprite_map "key1" "resources/sprites/godot.png" in
+  let atlas = make_atlas 96 96 16 16 in
+  let first_atlas = atlas.(0) in
   let render renderer =
     Render.clear renderer;
     Render.set_scale renderer (1.0, 1.0);
     Render.copy renderer ~texture:(SpriteMap.find "key1" sprite_map)
-      ~src_rect:(Rect.make4 ~x:0 ~y:0 ~w:16 ~h:16)
+      ~src_rect:(Rect.make4 ~x:first_atlas.x ~y:first_atlas.y ~w:16 ~h:16)
       ~dst_rect:(Rect.make4 ~x:100 ~y:100 ~w:32 ~h:32)
       ();
     Render.render_present renderer;
